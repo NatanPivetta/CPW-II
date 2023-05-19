0e8211e5df7218e353965152e7fc20958f01eb9a
Natan Pivetta <51028072+NatanPivetta@users.noreply.github.com>
2023-05-19 13:26:38 +0000

    Exercício Promises com Ajax

---
+++ Exercicios/AjaxPromises/index.js
@@ -0,0 +1,49 @@
+async function newRequest(method, url){
+    return new Promise ((res, rej) => {
+
+        var xml = new XMLHttpRequest();
+
+        xml.onreadystatechange = () => {
+            if (xml.readyState == 4 && xml.status == 200){
+                res(JSON.parse(xml.responseText));
+                console.log("Requsição com sucesso");
+            }else{
+            }
+        }
+
+        xml.open(method,url, true);
+
+        xml.send();
+
+    });
+}
+
+var url = "imagens.json";
+
+async function main(){
+
+    try{
+        let response = await newRequest('GET', url);
+        let img = response.animals[Math.floor(Math.random() * response.animals.length)]
+        addImage(img);
+    }catch (error){
+        console.log(error);
+    }
+    
+}
+
+function addImage(img){
+    var divImages = document.getElementById("images");
+    var tagImg = document.createElement("img");
+    tagImg.src = img.imagemUrl;
+    tagImg.alt = img.name;
+    divImages.appendChild(tagImg);
+}
+
+window.onscroll = function(ev) {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+      main();
+      console.log("scroll");
+    }
+  };
+
